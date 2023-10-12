a076ed133512d4435f8377194356574d61d00b3b
Obafemi Oluwatobiloba <obafemitobi438@gmail.com>
2023-10-12 12:36:36 +0000

    4

---
+++ 0x00-MySQL_Advanced/metal_bands.sql
@@ -0,0 +1,32 @@
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+DROP TABLE IF EXISTS `metal_bands`;
+CREATE TABLE `metal_bands` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `band_name` varchar(255) DEFAULT NULL,
+  `fans` int(11) DEFAULT NULL,
+  `formed` year DEFAULT NULL,
+  `origin` varchar(255) DEFAULT NULL,
+  `split` year DEFAULT NULL,
+  `style` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+INSERT INTO `metal_bands` (`band_name`, `fans`, `formed`, `id`, `origin`, `split`, `style`)
+VALUES ('Iron Maiden', '4195', '1975', '1', 'United Kingdom', NULL, 'New wave of british heavy,Heavy'),
+('Opeth', '4147', '1990', '2', 'Sweden', '1990', 'Extreme progressive,Progressive rock,Progressive'),
+('Metallica', '3712', '1981', '3', 'USA', NULL, 'Heavy,Bay area thrash'),
+('Megadeth', '3105', '1983', '4', 'USA', '1983', 'Thrash,Heavy,Hard rock'),
+('Amon Amarth', '3054', '1988', '5', 'Sweden', NULL, 'Melodic death'),
+('Slayer', '2955', '1981', '6', 'USA', '1981', 'Thrash'),
+('Death', '2690', '1983', '7', 'USA', '2001', 'Progressive death,Death,Progressive thrash'),
+('Dream Theater', '2329', '1985', '8', 'USA', '1985', 'Progressive'),
